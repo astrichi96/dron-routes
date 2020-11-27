480643b4441452e60538691a14a6952e5deae727
Astrid Vanegas <astrichi96@gmail.com>
2020-11-27 15:09:32 -0500

    add promise to handle the all file instructions at the same time

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ const {
 } = require('./constants');
 
 const main = () => {
-  try {
-    const filenames = readdir(INPUT_DIR);
-    for (const filename of filenames) {
-      const list = readFiles(`${INPUT_DIR}${filename}`);
-      const stream = writeFile(`${OUTPUT_DIR}${filename.replace('in', 'out')}`);
-      processingFiles(stream, list, INITIAL_VALUES);
+  return new Promise((resolve, reject) => {
+    try {
+      const filenames = readdir(INPUT_DIR);
+      for (const filename of filenames) {
+        const list = readFiles(`${INPUT_DIR}${filename}`);
+        const stream = writeFile(
+          `${OUTPUT_DIR}${filename.replace('in', 'out')}`
+        );
+        processingFiles(stream, list, INITIAL_VALUES);
+      }
+      console.log(
+        'The routes has been created, See the files in this path: dron-routes/output'
+      );
+      resolve();
+    } catch (error) {
+      if (error.message === DIRECTORIES_NOT_FOUND.INPUT)
+        console.log('Please create input folder with the files routes');
+      if (error.message === DIRECTORIES_NOT_FOUND.OUTPUT)
+        console.log('Please create output folder with the files routes');
+      reject(error);
     }
-  } catch (error) {
-    if (error.message === DIRECTORIES_NOT_FOUND.INPUT)
-      console.log('Please create input folder with the files routes');
-    if (error.message === DIRECTORIES_NOT_FOUND.OUTPUT)
-      console.log('Please create output folder with the files routes');
-  }
+  });
 };
 
 // Read all files from the input directory
