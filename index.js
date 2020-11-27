ea8c668227765155e9d6535682b6fb8cf22b4ec5
Astrid Vanegas <astrichi96@gmail.com>
2020-11-27 10:49:28 -0500

    create main function to start the project

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const { processingFiles } = require('./handler');
+const {
+  INPUT_DIR,
+  OUTPUT_DIR,
+  INITIAL_VALUES,
+  DIRECTORIES_NOT_FOUND
+} = require('./constants');
+
+const main = () => {
+  try {
+    const filenames = readdir(INPUT_DIR);
+    for (const filename of filenames) {
+      const list = readFiles(`${INPUT_DIR}${filename}`);
+      const stream = writeFile(`${OUTPUT_DIR}${filename.replace('in', 'out')}`);
+      processingFiles(stream, list, INITIAL_VALUES);
+    }
+  } catch (error) {
+    if (error.message === DIRECTORIES_NOT_FOUND.INPUT)
+      console.log('Please create input folder with the files routes');
+    if (error.message === DIRECTORIES_NOT_FOUND.OUTPUT)
+      console.log('Please create output folder with the files routes');
+  }
+};
+
+// Read all files from the input directory
+const readdir = (path) => {
+  if (!existsDirOrFile(path)) throw Error(DIRECTORIES_NOT_FOUND.INPUT);
+  return fs.readdirSync(path);
+};
+
+// Read file by file from input directory
+const readFiles = (path) => fs.readFileSync(path, 'utf8').split('\n');
+
+// Create stream to write in the file when the row is available
+const writeFile = (path) => {
+  if (!existsDirOrFile(OUTPUT_DIR)) throw Error(DIRECTORIES_NOT_FOUND.OUTPUT);
+  return fs.createWriteStream(path);
+};
+
+const existsDirOrFile = (filePath = '') => fs.existsSync(filePath);
+
+main();
