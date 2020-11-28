12be14cc95851f217df8e903c41df395e388f2c3
Astrid Vanegas <astrichi96@gmail.com>
2020-11-28 12:04:04 -0500

    Resolve multiple dron file instructions

--- deliveryRunner.js
+++ deliveryRunner.js
@@ -4,22 +4,24 @@ const handler = require('./handler');
 const { INPUT_DIR, OUTPUT_DIR, INITIAL_VALUES } = require('./constants');
 
 const main = async () => {
-  return new Promise((resolve, reject) => {
-    utils.createDirectory(OUTPUT_DIR);
-    try {
-      const filenames = utils.readdir(INPUT_DIR);
-      for (const filename of filenames) {
-        const inputPath = `${INPUT_DIR}${filename}`;
-        const outputPath = `${OUTPUT_DIR}${filename.replace('in', 'out')}`;
+  utils.createDirectory(OUTPUT_DIR);
+  try {
+    const filenames = utils.readdir(INPUT_DIR);
+    await Promise.all(
+      filenames.map((filename) => {
+        return new Promise((resolve, reject) => {
+          const inputPath = `${INPUT_DIR}${filename}`;
+          const outputPath = `${OUTPUT_DIR}${filename.replace('in', 'out')}`;
 
-        const list = utils.readFiles(inputPath);
-        handler.processRoutes(outputPath, list, INITIAL_VALUES);
-      }
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+          const list = utils.readFiles(inputPath);
+          handler.processRoutes(outputPath, list, INITIAL_VALUES);
+          resolve();
+        });
+      })
+    );
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = { main };
